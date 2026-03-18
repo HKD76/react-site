65ece8a1a03e42f8358794526a4e94bf7e3c0803
totorz76 <victor76.zammit@gmail.com>
2026-03-18 15:48:37 +0100

    Motion on LogicielHero

--- src/pages/LogicielPage/components/LogicielHero/LogicielHero.jsx
+++ src/pages/LogicielPage/components/LogicielHero/LogicielHero.jsx
@@ -1,27 +1,38 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { fadeInUpContainer, fadeInUpItem } from "../../../../animation/animation";
 import "./LogicielHero.css";
 
 export default function LogicielHero() {
   return (
     <section className="logiciel-hero">
-      <div className="page-container">
-        <h1 className="title">Logiciel MES/TRS personnalisable</h1>
-        <p className="page-subtitle">
+      <motion.div
+        className="page-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        <motion.h1 variants={fadeInUpItem} className="title">Logiciel MES/TRS personnalisable</motion.h1>
+        <motion.p variants={fadeInUpItem} className="page-subtitle">
           Dites Aurevoir aux relevés manuels et Bienvenue dans l'ère de
           l'industrie 4.0
-        </p>
-        <div className="logiciel-hero-buttons">
+        </motion.p>
+        <motion.div variants={fadeInUpItem} className="logiciel-hero-buttons">
           <a href="#logiciel-advantage" className="logiciel-hero-button">
             Les avantages
           </a>
           <Link to="/demander-une-demo" className="button-secondary">
             Demander une démo
           </Link>
-        </div>
-        <div className="logiciel-hero-image">
-            <img src="/images/suivi-production-temps-reel-p-1080.webp" alt="Suivi de production" />
-        </div>
-      </div>
+        </motion.div>
+        <motion.div variants={fadeInUpItem} className="logiciel-hero-image">
+          <img
+            src="/images/suivi-production-temps-reel-p-1080.webp"
+            alt="Suivi de production"
+          />
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
