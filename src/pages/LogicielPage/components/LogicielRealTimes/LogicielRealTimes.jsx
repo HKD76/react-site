c5a41d5a726c9d26a1067debc79a8934be43225b
totorz76 <victor76.zammit@gmail.com>
2026-03-18 16:03:51 +0100

    Motion on LogicielRealTimes

--- src/pages/LogicielPage/components/LogicielRealTimes/LogicielRealTimes.jsx
+++ src/pages/LogicielPage/components/LogicielRealTimes/LogicielRealTimes.jsx
@@ -1,38 +1,53 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 import "./LogicielRealTimes.css";
 
 export default function LogicielRealTimes() {
+  const MotionLink = motion(Link);
   return (
     <section className="logiciel-realtimes">
-      <div className="logiciel-realtimes-container">
+      <motion.div
+        className="logiciel-realtimes-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
         <div className="logiciel-realtimes-content">
-          <p className="logiciel-realtimes-label">RealTiMES</p>
-          <h2 className="logiciel-realtimes-title">
+          <motion.p variants={fadeInUpItem} className="logiciel-realtimes-label">RealTiMES</motion.p>
+          <motion.h2 variants={fadeInUpItem} className="logiciel-realtimes-title">
             Solution de suivie connectée et sécurisée
-          </h2>
-          <p className="logiciel-realtimes-text">
+          </motion.h2>
+          <motion.p variants={fadeInUpItem} className="logiciel-realtimes-text">
             Collecte de vos données de production automatique. Affichage en
             temps réel de la performance en atelier. Partage de l’information en
             temps réel dans toute l’usine.
-          </p>
-          <p className="logiciel-realtimes-text">
+          </motion.p>
+          <motion.p variants={fadeInUpItem} className="logiciel-realtimes-text">
             L'hébergement de vos données est sur votre serveur, mais une
             solution "CLOUD" existe sur demande
-          </p>
-          <p className="logiciel-realtimes-text">
+          </motion.p>
+          <motion.p variants={fadeInUpItem} className="logiciel-realtimes-text">
             Accès en illimité à toutes les données enregistrées sur la ligne.
             Rapports de performance personnalisables. Suivi des enregistrements
             composants et des contrôles.
-          </p>
-          <Link to="/demander-une-demo" className="logiciel-realtimes-button"> Demander une démo</Link>
+          </motion.p>
+          <MotionLink variants={fadeInUpItem} to="/demander-une-demo" className="logiciel-realtimes-button">
+            {" "}
+            Demander une démo
+          </MotionLink>
         </div>
-        <div className="logiciel-realtimes-image">
+        <motion.div variants={fadeInUpItem} className="logiciel-realtimes-image">
           <img
             src="/images/a-propos-realtimes_1a-propos-realtimes.webp"
             alt="A propos de Realtimes"
           />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
