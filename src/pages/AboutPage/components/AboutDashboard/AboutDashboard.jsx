00f3acaf6ba21918c4680b15cadce826e521550c
totorz76 <victor76.zammit@gmail.com>
2026-03-18 15:34:16 +0100

    Motion on AboutDashboard

--- src/pages/AboutPage/components/AboutDashboard/AboutDashboard.jsx
+++ src/pages/AboutPage/components/AboutDashboard/AboutDashboard.jsx
@@ -1,23 +1,35 @@
-import './AboutDashboard.css';
+import "./AboutDashboard.css";
+import { motion } from "framer-motion";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 
 export default function AboutDashboard() {
   return (
     <section className="about-dashboard">
-      <div className="about-dashboard-container">
-        <div className="about-dashboard-content">
+      <motion.div
+        className="about-dashboard-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        <motion.div variants={fadeInUpItem} className="about-dashboard-content">
           <h2 className="about-dashboard-title">
             Tableaux de bord envoyés par e-mail
           </h2>
           <p className="about-dashboard-text">
-            Avec la solution complète de suivi de production <span className="highlight">RealTiMES</span>, les
-            directeurs de production ont la possibilité de recevoir des
-            informations à date fixe en automatique sans actions de votre part.
+            Avec la solution complète de suivi de production{" "}
+            <span className="highlight">RealTiMES</span>, les directeurs de
+            production ont la possibilité de recevoir des informations à date
+            fixe en automatique sans actions de votre part.
           </p>
-        </div>
-        <div className="about-dashboard-image">
+        </motion.div>
+        <motion.div variants={fadeInUpItem} className="about-dashboard-image">
           <img src="/images/tableau-de-bord-p-1080.webp" alt="Dashboard" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
