2c2eccbf2f93f14daa9146b218ef62e972d270ba
totorz76 <victor76.zammit@gmail.com>
2026-03-18 14:46:12 +0100

    Motion on SupportSection

--- src/pages/Home/components/SupportSection/SupportSection.jsx
+++ src/pages/Home/components/SupportSection/SupportSection.jsx
@@ -1,38 +1,49 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { fadeInUpContainer, fadeInUpItem } from "../../../../animation/animation";
 import "./SupportSection.css";
 
 export default function SupportSection() {
   return (
     <section className="support-section">
-      <div className="container">
+      <motion.div
+        className="container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
         <div className="support-card">
-          <div className="support-image">
+          <motion.div variants={fadeInUpItem} className="support-image">
             <img
               src="/images/collaboration-graphic.png"
               alt="collaboration interne"
             />
-          </div>
+          </motion.div>
 
           <div className="support-content">
-            <h2><span className='highlight'>Un dashboard</span> multi-utilisateurs + support client</h2>
-            <p className="support-text">
+            <motion.h2 variants={fadeInUpItem}>
+              <span className="highlight">Un dashboard</span> multi-utilisateurs
+              + support client
+            </motion.h2>
+            <motion.p variants={fadeInUpItem} className="support-text">
               Le dashboard RealTiMES centralise les indicateurs clés pour tous
               les profils, du terrain à la direction. Associé à un support
               client réactif, il garantit un pilotage fiable et une utilisation
               optimale des données de production.
-            </p>
+            </motion.p>
 
-            <div className="support-highlight">
+            <motion.div variants={fadeInUpItem} className="support-highlight">
               <span className="support-highlight-title">Support dédié</span>
               <img src="/images/support.svg" alt="picto support" />
-            </div>
+            </motion.div>
 
-            <Link to="/clients/cas-clients" className="card-button">
+            <MotionLink to="/clients/cas-clients" className="card-button">
               Ils nous font confiance
-            </Link>
+            </MotionLink>
           </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
