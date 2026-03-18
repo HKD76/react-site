df3c8bc0fe58fe98db2c94e9be7e08d01bdd7390
totorz76 <victor76.zammit@gmail.com>
2026-03-18 15:58:05 +0100

    Motion on LogicielExample

--- src/pages/LogicielPage/components/LogicielExample/LogicielExample.jsx
+++ src/pages/LogicielPage/components/LogicielExample/LogicielExample.jsx
@@ -1,23 +1,34 @@
 import "./LogicielExample.css";
+import { motion } from "framer-motion";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 
 export default function LogicielExample() {
   return (
     <section className="logiciel-example">
-      <div className="logiciel-example-container">
-        <h2 className="logiciel-example-title">
+      <motion.div
+        className="logiciel-example-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        <motion.h2 variants={fadeInUpItem} className="logiciel-example-title">
           Exemple de tableau de bord de suivi TRS
-        </h2>
-        <p className="logiciel-example-text">
+        </motion.h2>
+        <motion.p variants={fadeInUpItem} className="logiciel-example-text">
           Le tableau de bord RealTiMES vous permet de visualiser en temps réel
           la disponibilité des machines, la performance, et la qualité de vos
           lignes de production. Cet affichage intuitif facilite l'identification
           des sources de perte et permet d’optimiser rapidement votre
           productivité.
-        </p>
-        <div className="logiciel-example-image">
+        </motion.p>
+        <motion.div variants={fadeInUpItem} className="logiciel-example-image">
           <img src="/images/Ecran_Loué.webp" alt="Ecran Loué" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
