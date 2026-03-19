564f7da587cb507e919c7f48debc121667daff8b
totorz76 <victor76.zammit@gmail.com>
2026-03-19 09:06:28 +0100

    Motion on ProjetHero

--- src/pages/ProjetPage/components/ProjetHero/ProjetHero.jsx
+++ src/pages/ProjetPage/components/ProjetHero/ProjetHero.jsx
@@ -1,30 +1,44 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 import "./ProjetHero.css";
 
 export default function ProjetHero() {
   return (
     <section className="projet-hero">
-      <div className="projet-hero-container">
+      <motion.div
+        className="projet-hero-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
         <div className="projet-hero-content">
-          <h2 className="projet-hero-title">Votre <span className="highlight">projet MES</span> sur projet MES en 4 étapes</h2>
-          <p className="projet-hero-text">
+          <motion.h2 variants={fadeInUpItem} className="projet-hero-title">
+            Votre <span className="highlight">projet MES</span> sur projet MES
+            en 4 étapes
+          </motion.h2>
+          <motion.p variants={fadeInUpItem} className="projet-hero-text">
             Vous cherchez à optimiser votre système de production ? Découvrez
             notre solution numérique d'amélioration de la performance sur votre
             ligne de production.
-          </p>
-          <div className="projet-hero-buttons">
+          </motion.p>
+          <motion.div variants={fadeInUpItem} className="projet-hero-buttons">
             <a className="projet-hero-button" href="#timeline">
               Les étapes
             </a>
             <Link to="demander-une-demo" className="button-secondary">
               Demander une demo
             </Link>
-          </div>
+          </motion.div>
         </div>
-        <div className="projet-hero-image">
+        <motion.div variants={fadeInUpItem} className="projet-hero-image">
           <img src="/images/projet-mes-realtimes-p-500.webp" alt="Projet MES" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
