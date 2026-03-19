3366de80a033964899a2ab1d72c59158382f8f77
totorz76 <victor76.zammit@gmail.com>
2026-03-19 09:31:14 +0100

    Motion on CasHero

--- src/pages/CasPage/components/CasHero/CasHero.jsx
+++ src/pages/CasPage/components/CasHero/CasHero.jsx
@@ -1,26 +1,57 @@
 import "./CasHero.css";
+import { motion } from "framer-motion";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 
 export default function CasHero() {
   return (
     <section className="cas-hero">
-      <div className="cas-hero-container">
-        <h2 className="title">
+      <motion.div
+        className="cas-hero-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        <motion.h2 variants={fadeInUpItem} className="title">
           Cas clients RealTiMES : le suivi de performance par secteur
-        </h2>
-        <p className="cas-hero-text">
+        </motion.h2>
+        <motion.p variants={fadeInUpItem} className="cas-hero-text">
           Nos clients industriels sont confrontées à plusieurs défis en matière
-          de <span className="highlight">lean management</span> et de <span className="highlight">lean manufacturing :</span>
-        </p>
+          de <span className="highlight">lean management</span> et de{" "}
+          <span className="highlight">lean manufacturing :</span>
+        </motion.p>
         <ul>
-          <li>- Gestion efficace des flux de production (réduire le gaspillage, le temps d'attente, optimiser la productivité)</li>
-          <li>- Gestion des stocks</li>
-          <li>- Gestion de la qualité (et sécurité alimentaire)</li>
-          <li>- Flexibilité opérationnelle</li>
-          <li>- Implication du personnel</li>
+          <motion.li className="cas-hero-text" variants={fadeInUpItem}>
+            - Gestion efficace des flux de production (réduire le gaspillage, le
+            temps d'attente, optimiser la productivité)
+          </motion.li>
+          <motion.li variants={fadeInUpItem} className="cas-hero-text">
+            - Gestion des stocks
+          </motion.li>
+          <motion.li variants={fadeInUpItem} className="cas-hero-text">
+            - Gestion de la qualité (et sécurité alimentaire)
+          </motion.li>
+          <motion.li variants={fadeInUpItem} className="cas-hero-text">
+            - Flexibilité opérationnelle
+          </motion.li>
+          <motion.li variants={fadeInUpItem} className="cas-hero-text">
+            - Implication du personnel
+          </motion.li>
         </ul>
-        <p className="cas-hero-text">Notre rôle est de leur apporter une flexibilité opérationnelle grâce à un suivi de production en temps réel tout en mettant à leur disposition des outils de management visuel afin d'impliquer facilement leur personnel.</p>
-        <p className="cas-hero-text"><span className="highlight">RealTiMES</span> permet à tous ces acteurs de répondre aux exigences de leurs marchés.</p>
-      </div>
+        <motion.p variants={fadeInUpItem} className="cas-hero-text">
+          Notre rôle est de leur apporter une flexibilité opérationnelle grâce à
+          un suivi de production en temps réel tout en mettant à leur
+          disposition des outils de management visuel afin d'impliquer
+          facilement leur personnel.
+        </motion.p>
+        <motion.p variants={fadeInUpItem} className="cas-hero-text">
+          <span className="highlight">RealTiMES</span> permet à tous ces acteurs
+          de répondre aux exigences de leurs marchés.
+        </motion.p>
+      </motion.div>
     </section>
   );
 }
