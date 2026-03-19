e70577fc9588816875f6899783d1063d44dc1ce0
totorz76 <victor76.zammit@gmail.com>
2026-03-19 09:16:45 +0100

    Motion on MediaHero

--- src/pages/MediaPage/components/MediaHero/MediaHero.jsx
+++ src/pages/MediaPage/components/MediaHero/MediaHero.jsx
@@ -1,25 +1,38 @@
 import "./MediaHero.css";
+import { motion } from "framer-motion";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 
 export default function MediaHero() {
   return (
-    <section className="media-hero">
+    <motion.section
+      className="media-hero"
+      variants={fadeInUpContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true }}
+    >
       <div className="media-hero-container">
-        <h2 className="title">Media</h2>
-        <p className="page-subtitle">Parutions : Presse et Vidéos</p>
+        <motion.h2 variants={fadeInUpItem} className="title">Media</motion.h2>
+        <motion.p variants={fadeInUpItem} className="page-subtitle">Parutions : Presse et Vidéos</motion.p>
       </div>
       <div className="media-hero-content">
-        <div className="media-hero-card">
+        <motion.div variants={fadeInUpItem} className="media-hero-card">
           <div className="media-hero-card-content">
             <h3 className="media-hero-card-title">
               Découvrez (bientôt) notre chaîne Youtube
             </h3>
-            <a className="media-hero-card-button" href="#">Chaîne YT RealTiMES</a>
+            <a className="media-hero-card-button" href="#">
+              Chaîne YT RealTiMES
+            </a>
           </div>
           <div className="media-hero-card-image">
             <img src="/images/reproduction-logo.svg" alt="Logo RealTiMES" />
           </div>
-        </div>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 }
