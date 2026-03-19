884158c92c839e15ffb4d5a984139ee01443c958
totorz76 <victor76.zammit@gmail.com>
2026-03-19 09:25:25 +0100

    Motion on TestimonialPage

--- src/pages/TestimonialPage/TestimonialPage.jsx
+++ src/pages/TestimonialPage/TestimonialPage.jsx
@@ -2,23 +2,31 @@ import CallToActionSection from "../../components/CallToActionSection/CallToActi
 import ClientsSlider from "../../components/ClientsSlider/ClientsSlider";
 import TestimonialAptar from "./components/TestimonialAptar/TestimonialAptar";
 import TestimonialData from "./components/TestimonialData/TestimonialData";
+import { motion } from "framer-motion";
 import { clientLogos } from "../../data/clientsLogos";
+import { fadeInUpContainer, fadeInUpItem } from "../../animation/animation";
 
 export default function TestimonialPage() {
   return (
     <section>
-      <div className="page-background">
+      <motion.div
+        className="page-background"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
         <div className="page-container">
-          <h2 className="title">
+          <motion.h2 variants={fadeInUpItem} className="title">
             Gain de productivité jusqu'à 30%, ils témoignent
-          </h2>
-          <p className="page-subtitle">
+          </motion.h2>
+          <motion.p variants={fadeInUpItem} className="page-subtitle">
             Piloter la performance de leur production n'a jamais été aussi
             simple
-          </p>
+          </motion.p>
         </div>
-      <ClientsSlider logos={clientLogos} />
-      </div>
+        <ClientsSlider logos={clientLogos} />
+      </motion.div>
       <img src="/images/gray-bottom-shape.svg" alt="gray-bottom-shape" />
       <div className="page-container">
         <TestimonialAptar />
--- src/pages/TestimonialPage/components/TestimonialAptar/TestimonialAptar.jsx
+++ src/pages/TestimonialPage/components/TestimonialAptar/TestimonialAptar.jsx
@@ -1,43 +1,68 @@
 import "./TestimonialAptar.css";
+import { motion } from "framer-motion";
 import { useState } from "react";
+import {
+  fadeInUpContainer,
+  fadeInUpItem,
+} from "../../../../animation/animation";
 
 export default function TestimonialAptar() {
   const [open, setOpen] = useState(false);
   return (
     <section className="testimonial-aptar">
-      <div className="testimonial-aptar-container">
+      <motion.div
+        className="testimonial-aptar-container"
+        variants={fadeInUpContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
         <div className="testimonial-aptar-content">
-          <h2 className="testimonial-aptar-title">Témoignage Aptar</h2>
-          <p className="testimonial-aptar-text">
+          <motion.h2
+            variants={fadeInUpItem}
+            className="testimonial-aptar-title"
+          >
+            Témoignage Aptar
+          </motion.h2>
+          <motion.p variants={fadeInUpItem} className="testimonial-aptar-text">
             Spécialisée dans la conception et la fabrication de systèmes de
             distribution pour conditionnement de produits cosmétiques, Aptar
             emploie quelque 300 personnes sur son site de Verneuil sur Avre. Un
             site qui réunit trois entités et trois secteurs d’activité
             différents :
-          </p>
+          </motion.p>
           <ul>
-            <li className="testimonial-aptar-text">
+            <motion.li
+              variants={fadeInUpItem}
+              className="testimonial-aptar-text"
+            >
               Personnal Care (mass market),
-            </li>
-            <li className="testimonial-aptar-text">
+            </motion.li>
+            <motion.li
+              variants={fadeInUpItem}
+              className="testimonial-aptar-text"
+            >
               Gift and promotion (échantillons et produits de promotion),
-            </li>
-            <li className="testimonial-aptar-text">
+            </motion.li>
+            <motion.li
+              variants={fadeInUpItem}
+              className="testimonial-aptar-text"
+            >
               Airlessystems (cosmétique de luxe).
-            </li>
+            </motion.li>
           </ul>
-          <p className="testimonial-aptar-text">
+          <motion.p variants={fadeInUpItem} className="testimonial-aptar-text">
             <span className="highlight">Aptar</span> réalise un chiffre
             d’affaires de 100M€ par an.{" "}
             <span className="highlight"> Realtimes </span>
             équipe 49 machines sur l’entité « Personnal Care » pour piloter la
             performance.
-          </p>
+          </motion.p>
         </div>
-        <div className="testimonial-aptar-image">
+        <motion.div variants={fadeInUpItem} className="testimonial-aptar-image">
           <img src="/images/LogoAptarPharma_1.webp" alt="Logo Aptar" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
       <div
         className="testimonial-aptar-subcontent"
         onClick={() => setOpen(!open)}
@@ -55,53 +80,61 @@ export default function TestimonialAptar() {
           Pourquoi choisir Realtimes pour piloter la performance ?
         </p>
         <p className="testimonial-aptar-interview-author">
-            <span className="highlight">Marc Leriche</span>, responsable engineering et maintenance
+          <span className="highlight">Marc Leriche</span>, responsable
+          engineering et maintenance
         </p>
         <p className="testimonial-aptar-interview-text">
-          « Cela remonte à 2007, j’étais responsable maintenance chez <span className="highlight">Personnal Care</span> et chaque jours, j’entendais que des choses ne fonctionnaient
-          pas. C’était particulièrement frustrant car je savais par ailleurs
-          qu’il y avait des soucis d’organisation sur les machines mais je ne
-          parvenais pas à les identifier et à les quantifier. Ceci dit, il est
-          toujours plus facile de dire qu’une machine ne fonctionne pas plutôt
-          que de chercher les causes du dysfonctionnement. J’avais donc un
-          besoin essentiel : identifier les problèmes. J’ai rencontré <span className="highlight">Jean-Marc
-          Sigaudy</span> qui m’a proposé <span className="highlight">Realtimes</span> et sur le papier, la solution était
-          trouvée. Nous allions avoir les moyens d’identifier les causes des
-          problèmes qu’ils soient humains ou techniques et surtout, de pouvoir
-          faire le rapport problème – coût induit. Nous avions besoin d’un
-          système simple à utiliser, réactif et flexible. <span className="highlight">Realtimes</span> répond à
-          l’ensemble de ces critères ».
+          « Cela remonte à 2007, j’étais responsable maintenance chez{" "}
+          <span className="highlight">Personnal Care</span> et chaque jours,
+          j’entendais que des choses ne fonctionnaient pas. C’était
+          particulièrement frustrant car je savais par ailleurs qu’il y avait
+          des soucis d’organisation sur les machines mais je ne parvenais pas à
+          les identifier et à les quantifier. Ceci dit, il est toujours plus
+          facile de dire qu’une machine ne fonctionne pas plutôt que de chercher
+          les causes du dysfonctionnement. J’avais donc un besoin essentiel :
+          identifier les problèmes. J’ai rencontré{" "}
+          <span className="highlight">Jean-Marc Sigaudy</span> qui m’a proposé{" "}
+          <span className="highlight">Realtimes</span> et sur le papier, la
+          solution était trouvée. Nous allions avoir les moyens d’identifier les
+          causes des problèmes qu’ils soient humains ou techniques et surtout,
+          de pouvoir faire le rapport problème – coût induit. Nous avions besoin
+          d’un système simple à utiliser, réactif et flexible.{" "}
+          <span className="highlight">Realtimes</span> répond à l’ensemble de
+          ces critères ».
         </p>
         <p className="testimonial-aptar-interview-subtitle">
           « Realtimes est un outil d’amélioration continue »
         </p>
         <p className="testimonial-aptar-interview-text">
           « Souvent, dans les entreprises il n’y a pas de connexion entre la
-          maintenance et la production. <span className="highlight">Realtimes</span> a permis de combler ce vide
-          grâce à un tableau de bord qui nous permet de suivre l’ensemble de
-          notre parc machines en temps réel.
+          maintenance et la production.{" "}
+          <span className="highlight">Realtimes</span> a permis de combler ce
+          vide grâce à un tableau de bord qui nous permet de suivre l’ensemble
+          de notre parc machines en temps réel.
         </p>
         <p className="testimonial-aptar-interview-text">
           De plus, les tableaux de bord sont évolutifs en fonction de nos
-          besoins. <span className="highlight">RealTimes</span> est en mesure de les faire évoluer à la demande.
-          L’arrivée de <span className="highlight">Realtimes</span> a été plutôt bien perçue sur les unités de
-          production. Évidemment, un système comme celui-là ne sert à quelque
-          chose que si tout le monde s’implique. Nous avons gagné en réactivité
-          et cela nous a amenés à revoir notre management en installant
-          notamment des écrans dans l’atelier qui permettent à chacun de voir ce
-          qui se passe. Nous montrons tout, ce qui ne va pas et bien sûr, ce qui
-          va bien.{" "}
+          besoins. <span className="highlight">RealTimes</span> est en mesure de
+          les faire évoluer à la demande. L’arrivée de{" "}
+          <span className="highlight">Realtimes</span> a été plutôt bien perçue
+          sur les unités de production. Évidemment, un système comme celui-là ne
+          sert à quelque chose que si tout le monde s’implique. Nous avons gagné
+          en réactivité et cela nous a amenés à revoir notre management en
+          installant notamment des écrans dans l’atelier qui permettent à chacun
+          de voir ce qui se passe. Nous montrons tout, ce qui ne va pas et bien
+          sûr, ce qui va bien.{" "}
         </p>
         <p className="testimonial-aptar-interview-text">
-          L’avantage de <span className="highlight">Realtimes</span>, c’est qu’il donne toujours une explication
-          précise au problème rencontré et que le problème en question ne peut
-          faire l’objet d’aucune interprétation. Il permet également de savoir
-          quelle production passe mal sur les machines et de fait, d’anticiper
-          sur les mesures à prendre pour limiter en limiter les effets néfastes.
-          Si je devais résumer, je dirais que <span className="highlight">Realtimes</span> nous a apporté un gain
-          d’organisation beaucoup plus flagrant qu’en technique pure. Notre taux
-          de rentabilité synthétique (TRS) a en effet progressé de 15 points. Je
-          le qualifie d’outil d’amélioration continue ».{" "}
+          L’avantage de <span className="highlight">Realtimes</span>, c’est
+          qu’il donne toujours une explication précise au problème rencontré et
+          que le problème en question ne peut faire l’objet d’aucune
+          interprétation. Il permet également de savoir quelle production passe
+          mal sur les machines et de fait, d’anticiper sur les mesures à prendre
+          pour limiter en limiter les effets néfastes. Si je devais résumer, je
+          dirais que <span className="highlight">Realtimes</span> nous a apporté
+          un gain d’organisation beaucoup plus flagrant qu’en technique pure.
+          Notre taux de rentabilité synthétique (TRS) a en effet progressé de 15
+          points. Je le qualifie d’outil d’amélioration continue ».{" "}
         </p>
       </div>
     </section>
